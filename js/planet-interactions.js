3da26587b428a9331e430354fbebb788bb67078a
Stervar <150718158+Stervar@users.noreply.github.com>
2024-12-24 23:16:11 +0300

    Продолжаю создовать пушку

--- js/planet-interactions.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.close-modal').addEventListener('click', () => {
-    document.getElementById('planet-modal').style.display = 'none';
-});
-
-window.onclick = function(event) {
-    if (event.target == document.getElementById('planet-modal')) {
-        document.getElementById('planet-modal').style.display = 'none';
-    }
-};

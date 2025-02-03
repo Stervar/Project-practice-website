0aced6de188ca1e5d4dca07fb9b3fc1d946b0418
❃𝘝𝘢𝘥𝘪𝘮_𝘎𝘢𝘣𝘦𝘳𝘬𝘰𝘳𝘯❃ <150718158+Stervar@users.noreply.github.com>
2025-02-03 22:02:12 +0300

    gsnrb

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+  
+def index(request):
+    return HttpResponse("Главная")
+ 
+def about(request):
+    return HttpResponse("О сайте")
+ 
+def contact(request):
+    return HttpResponse("Контакты")
